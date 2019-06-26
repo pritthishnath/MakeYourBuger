24191f825d5a610ddce16bdef4333b1598003068
Pritthish Nath <pritthishnath@gmail.com>
2019-06-27 02:13:09 +0530

    Initial overview

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends React.Component {
+    render () {
+        return (
+            <Aux>
+                <div>Burger</div>
+                <div>Builder Controls</div>
+            </Aux>
+        )
+    }
+}
+
+export default BurgerBuilder;
